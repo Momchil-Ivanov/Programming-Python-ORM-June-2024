6c2e9e7a097e5a5c5746e2816443c76da20eebed
Momchil Ivanov <88820552+Momchil-Ivanov@users.noreply.github.com>
2024-06-22 19:36:01 +0300

    Add files via upload

---
+++ 06.migrations_and_django_admin/04.customize_the_admin/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=500)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_edited_on = models.DateTimeField(auto_now=True)
+    category = models.CharField(max_length=100, null=False, blank=False)
+    supplier = models.CharField(max_length=100, null=False, blank=False)
+    count = models.IntegerField(default=0)
+    barcode = models.IntegerField()
+
+    def __str__(self):
+        return self.name
